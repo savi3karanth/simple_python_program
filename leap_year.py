12fdabb4e027bf1bb644a4532ab3c46e3d46a48c
savi3karanth <karanth.savi3@gmail.com>
2025-02-13 14:13:27 -0500

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,7 +2,7 @@ year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
     if year % 100 == 0:
-        if year % 400 == :
+        if year % 400 == 0:
             print("leap year")
         else:
             print("not leap year")
